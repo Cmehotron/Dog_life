28bedd53ddf9f5d400cf738bc9c3792bb66cc9c4
Cmehotron <lex_1488@mail.ru>
2022-11-26 12:36:54 +0300

    Menu 2 it
    Signed-off-by: Cmehotron <lex_1488@mail.ru>

--- app/src/main/java/com/example/dog_life/MainActivity.kt
+++ app/src/main/java/com/example/dog_life/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.dog_life
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.ui.*
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,22 +14,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // устанавливаем свою реализацию тулбара
-      //  setSupportActionBar(toolbar)
-
-       // val navController = findNavController(R.id.fragment_container)
-
-        // конфигурация тулбара
-        //appBarConfig = AppBarConfiguration(navController.graph, drawer_layout)
-        //setupActionBarWithNavController(navController, appBarConfig)
-
-        // метод, который связывает шторку с навигацией
-        //nav_view.setupWithNavController(navController)
     }
-
-    // отслеживает клик по иконке гамбургера и стрелке UP
-   // override fun onSupportNavigateUp(): Boolean {
-    //    val navController = findNavController(R.id.fragment_container)
-   //     return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
-  //  }
 }
